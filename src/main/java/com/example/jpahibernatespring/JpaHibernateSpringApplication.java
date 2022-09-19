8473a9d64bcdab6e06e0ce4f4f81d9c6d422131e
DMSofwareDev <89517604+SuperDMWebDev@users.noreply.github.com>
2022-09-19 09:55:25 +0700

    first commit

---
+++ src/main/java/com/example/jpahibernatespring/JpaHibernateSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.jpahibernatespring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaHibernateSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaHibernateSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/jpahibernatespring/JpaHibernateSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jpahibernatespring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaHibernateSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
